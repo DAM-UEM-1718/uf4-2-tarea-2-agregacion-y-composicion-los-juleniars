8d0b8078e977396457c839bd6d24c80d36c9840c
julenb <21701719@live.uem.es>
2018-04-10 16:20:33 +0200

    Initial commit

---
+++ src/enunciado3/Libro.java
@@ -0,0 +1,46 @@
+package enunciado3;
+
+public class Libro {
+
+    private String titulo;
+
+    private long isbn;
+
+    private int anoPublicacion;
+
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnoPublicacion() {
+        return anoPublicacion;
+    }
+
+    public void setAnoPublicacion(int anoPublicacion) {
+        this.anoPublicacion = anoPublicacion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    
+}
