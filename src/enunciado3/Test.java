3a2644558bf4e91e049b69b20923d0aa696118ac
julenb <21701719@live.uem.es>
2018-04-10 16:53:44 +0200

    Enunciado 3

--- src/enunciado3/Test.java
+++ src/enunciado3/Test.java
@@ -6,7 +6,8 @@ public class Test {
         Libro libro = new Libro("Lo Que El Viento Se Llevó", 9784003234242L, 1936, "Margaret Mitchell");
         libro.addPagina("Lorem ipsum dolor sit amet, sit praesent torquatos ea," +
                 " adipiscing eloquentiam cum eu, an pri partem noster. Eam at hinc aeque pertinacia," +
-                " qui fugit deleniti vivendum te. Eros choro conceptam quo ei, erant animal corrumpit " +
+                " qui fugit deleniti vivendum te");
+        libro.addPagina("Eros choro conceptam quo ei, erant animal corrumpit " +
                 "vis at, ea mea euismod voluptua. An cum wisi inani noster.");
         System.out.println(libro);
     }
